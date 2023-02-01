6de7ef8e2d1eebbcc766a1d2a393e689ca035f19
Gurram, Pavan <pavan.gurram@legato.com>
2023-02-01 15:27:26 +0530

    modified

--- runs.py
+++ runs.py
@@ -6,4 +6,4 @@ l1_ratios=np.linspace(0.1, 1.0, 5)
 
 for alpha in alpha_s:
     for l1 in l1_ratios:
-        os.system(f"python Simple_ML_model.py -a {alpha} -l1 {l1}")
+        os.system(f"python simple_ML_model.py -a {alpha} -l1 {l1}")
